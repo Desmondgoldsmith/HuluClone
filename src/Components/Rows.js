1c7a3a255f91aa57afaa2de16edef77693ce6813
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-18 23:31:27 +0000

    Fetching movies from TMDB 🧨

--- src/Components/Rows.js
+++ src/Components/Rows.js
@@ -1,28 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import axios from '../Axios';
+import './Rows.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
+function Rows({fetchUrl}) {
+  const [movies,setMovies] = useState([]);
+  useEffect(()=>{
+    async function fetchData(){
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
 
-function Rows({fetchUrl}){
-    const [movies,setMovies] = useState([]);
-    useEffect(()=>{
-      async function fetchData(){
-        const request = await axios.get(fetchUrl);
-        setMovies(request.data.results);
-        return request;
-      }
-      fetchData();
-    },[fetchUrl])
+      return request;
+    }
+    fetchData();
+  },[fetchUrl])
   return (
-    <div className='row'>     
-       <div className='row_poster'>
-        {movies.map(movie=>(
-          <img 
-          className = 'poster_image'
-           src = {`${base_url}${movie.poster_path}`}
-           alt = {movie.name} />
-        ))}
-       </div>
+    <div className='row'>
+     <div className='row_poster'>
+       {
+        movies.map(movie=>(
+          <img
+           className='image_poster'
+           src={`${base_url}${movie.poster_path}`}
+           alt = {movie.name} 
+           />
+        ))
+       }
+     </div>
     </div>
   )
 }
