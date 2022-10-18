a613f2e607026961df666a6d2b6d41b6c74a179f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-18 22:32:51 +0000

    Hulu Init🐤

---
+++ src/Components/Rows.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Rows() {
+  return (
+    <div className='row'>     
+       <div className='row_poster'>
+       </div>
+    </div>
+  )
+}
+
+export default Rows
