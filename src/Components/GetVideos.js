c17373518f436ee95f47a4c8b5034d8d8baed63e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-19 15:05:13 +0000

    ADDING MOVIES 🚀

--- src/Components/GetVideos.js
+++ src/Components/GetVideos.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './GetVideos.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 function GetVideos({movie}) {
@@ -7,8 +8,8 @@ function GetVideos({movie}) {
         <img src={`${base_url}${movie.backdrop_path}`}
          alt = {movie.name}
          />
-         <p>This movie is about killing 👹 </p>
-         <h2>MOvie Title</h2>
+         <p>{movie.overview} </p>
+         <h2>{movie.name}</h2>
          <p>no of likes</p>
     </div>
   )
