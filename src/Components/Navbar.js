f5a447fd55ea648199bc6854c742a2d246cde54b
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-19 13:47:04 +0000

    STYLING NAVBAR 🚀

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,17 +4,17 @@ import './Navbar.css';
 function Navbar() {
   return (
     <div className='navbar'>
-    <h2 className='navbar_items'>Trending</h2>
-    <h2 className='navbar_items'>Top Rated</h2>
-    <h2 className='navbar_items'>Action</h2>
-    <h2 className='navbar_items'>Comedy</h2>
-    <h2 className='navbar_items'>Horror</h2>
-    <h2 className='navbar_items'>Romance</h2>
-    <h2 className='navbar_items'>Mystry</h2>
-    <h2 className='navbar_items'>Sci-fi</h2>
-    <h2 className='navbar_items'>Western</h2>
-    <h2 className='navbar_items'>Animation</h2>
-    <h2 className='navbar_items'>Movie</h2>
+    <h3 className='navbar_items'>Trending</h3>
+    <h3 className='navbar_items'>Top Rated</h3>
+    <h3 className='navbar_items'>Action</h3>
+    <h3 className='navbar_items'>Comedy</h3>
+    <h3 className='navbar_items'>Horror</h3>
+    <h3 className='navbar_items'>Romance</h3>
+    <h3 className='navbar_items'>Mystry</h3>
+    <h3 className='navbar_items'>Sci-fi</h3>
+    <h3 className='navbar_items'>Western</h3>
+    <h3 className='navbar_items'>Animation</h3>
+    <h3 className='navbar_items'>Movie</h3>
     </div>
   )
 }
